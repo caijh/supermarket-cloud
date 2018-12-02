5040d0def14d7ece984e2c9d06af74a0d7064ac1
caijh <caiqizhe@gmail.com>
2018-12-02 13:41:25 +0800

    update domain-product-model

---
+++ domain-product-model/src/main/java/com/coding/supermarket/domain/product/vo/ProductVo.java
@@ -0,0 +1,40 @@
+package com.coding.supermarket.domain.product.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import com.coding.commons.domain.brand.model.Brand;
+import com.coding.commons.domain.product.model.Category;
+import com.coding.commons.domain.product.model.ProductSku;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductVo implements Serializable {
+    private Long id;
+
+    private String name;
+
+    private String briefs;
+
+    private Category category;
+
+    private Brand brand;
+
+    private List<String> thumbnails;
+
+    private String description;
+
+    private Float taxRate;
+
+    private Integer status;
+
+    private List<ProductSku> skuList;
+
+    private Long createdBy;
+    private Date createTime;
+    private Long updatedBy;
+    private Date updateTime;
+}
