e89761769272e0e014e66924bceb5d94bef377c9
蔡俊辉 <caijunhui@pcitech.com>
2021-05-14 16:58:41 +0800

    refactor

--- service-product/src/main/java/com/github/caijh/supermarket/product/ServiceProductApplication.java
+++ service-product/src/main/java/com/github/caijh/supermarket/product/ServiceProductApplication.java
@@ -2,10 +2,8 @@ package com.github.caijh.supermarket.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class ServiceProductApplication {
 
     public static void main(String[] args) {
